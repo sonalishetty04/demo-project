3d8f30f7e0a3c570128fda686739e7923794f010
sonalishetty04 <sonali@appinessworld.com>
2024-06-28 15:36:36 +0530

    changed emergencycontact bg

--- src/components/EmergencyContact.jsx
+++ src/components/EmergencyContact.jsx
@@ -3,7 +3,7 @@ import { CiPhone } from "react-icons/ci";
 
 function EmergencyContact() {
   return (
-    <div className="bg-teal-500 flex gap-12 items-center pt-20 md:px-20">
+    <div className="bg-[url('https://img.freepik.com/free-photo/abstract-luxury-gradient-blue-background-smooth-dark-blue-with-black-vignette-studio-banner_1258-63889.jpg?w=1380&t=st=1719569028~exp=1719569628~hmac=9786f9d34a6c56baeb30766e5c5403b0e7e125292509d669879baef1647079e3')] bg-no-repeat bg-cover flex gap-12 items-center pt-12 md:px-20">
       <img
         src="https://www.lifesonmanipal.com/_next/image?url=%2Fimages%2FhomepageEmergencyContact2.webp&w=640&q=75 "
         alt="doctor"
